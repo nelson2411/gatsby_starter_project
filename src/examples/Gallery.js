4f8b20ff01b03bd364ca4ccef3fcf8a9268d600f
Nelson Rosales <nelson.rosales83@gmail.com>
2022-06-24 12:20:11 +0200

    feat: midsommar commit

--- src/examples/Gallery.js
+++ src/examples/Gallery.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import { GatsbyImage } from "gatsby-plugin-image"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import styled from "styled-components"
 
 // useStaticQuery and graphql must be imported from gatsby
 const query = graphql`
@@ -22,12 +23,37 @@ const query = graphql`
 
 const Gallery = () => {
   const data = useStaticQuery(query)
+  const nodes = data.allFile.nodes
   console.log(data)
   return (
-    <div>
-      <h2>Simple image gallery</h2>
-    </div>
+    <Wrapper>
+      {nodes.map((image, index) => {
+        const { name } = image
+        const pathToImage = getImage(image)
+
+        return (
+          <article key={index} className="item">
+            <GatsbyImage
+              image={pathToImage}
+              alt={name}
+              className="gallery-img"
+            />
+            <p>{name}</p>
+          </article>
+        )
+      })}
+    </Wrapper>
   )
 }
 
+const Wrapper = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  .item {
+    margin-right: 1rem;
+  }
+  .gallery-img {
+    border-radius: 1rem;
+  }
+`
 export default Gallery
