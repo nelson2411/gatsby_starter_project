b464e603177b2e871ee045fa757cb9ff264612f1
Nelson Rosales <nelson.rosales83@gmail.com>
2022-07-31 09:34:12 +0200

    feat: tag list implemented

---
+++ src/utils/setupTags.js
@@ -0,0 +1,22 @@
+const setupTags = recipes => {
+  const allTags = {}
+
+  recipes.forEach(recipe => {
+    recipe.content.tags.forEach(tag => {
+      if (allTags[tag]) {
+        allTags[tag] = allTags[tag] + 1
+      } else {
+        allTags[tag] = 1
+      }
+    })
+  })
+  const newtags = Object.entries(allTags).sort((a, b) => {
+    const [firstTag] = a
+    const [secondTag] = b
+    return firstTag.localeCompare(secondTag)
+  })
+
+  return newtags
+}
+
+export default setupTags
