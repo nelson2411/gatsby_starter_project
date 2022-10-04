c631620417995ca8dc1f99949f2f967d51b833e5
Nelson Rosales <nelson.rosales83@gmail.com>
2022-10-04 15:38:28 +0200

    feat: slugify added

---
+++ src/pages/{ContentfulRecipe.title}.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const RecipeTemplate = props => {
+  console.log(props)
+  return (
+    <div>
+      <h1>Recipe template</h1>
+    </div>
+  )
+}
+
+export default RecipeTemplate
